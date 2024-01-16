43c154b61f00a517c755ddb82356078ba5f09a4c
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-01-16 16:48:08 -0400

    Cria controller de Planet

---
+++ src/main/java/com/macedo/swplanetapi/web/PlanetController.java
@@ -0,0 +1,22 @@
+package com.macedo.swplanetapi.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.macedo.swplanetapi.domain.Planet;
+
+@RestController
+@RequestMapping("/planets")
+public class PlanetController {
+
+    @Autowired
+    PlanetService PlanetService;
+
+    @PostMapping
+    public ResponseEntity<Planet> create(@RequestBody Planet planet) {
+    }
+}
