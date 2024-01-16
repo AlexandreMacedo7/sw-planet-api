f7c6695b82f955e5adf99c5fdc7300a9c665ce7d
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-01-16 16:36:56 -0400

    Cria projeteo

---
+++ src/test/java/com/macedo/swplanetapi/SwPlanetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macedo.swplanetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwPlanetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
