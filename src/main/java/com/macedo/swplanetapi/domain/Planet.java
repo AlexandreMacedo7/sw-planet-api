171b1d736eb41fff686600c995ee26a763b7ab08
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-01-16 16:40:37 -0400

    Adiciona anotações para entidade

--- src/main/java/com/macedo/swplanetapi/domain/Planet.java
+++ src/main/java/com/macedo/swplanetapi/domain/Planet.java
@@ -1,39 +1,51 @@
 package com.macedo.swplanetapi.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "planets")
 public class Planet {
-    
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String name;
     private String climente;
     private String terrain;
-    
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getClimente() {
         return climente;
     }
+
     public void setClimente(String climente) {
         this.climente = climente;
     }
+
     public String getTerrain() {
         return terrain;
     }
+
     public void setTerrain(String terrain) {
         this.terrain = terrain;
     }
 
-    
 }
