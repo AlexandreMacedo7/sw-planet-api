82a7ca9bd6ef10d6d245b2ca344fe5364ae78d88
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-01-17 10:57:01 -0400

    Cria método para teste de dados inválidos

--- src/test/java/com/macedo/swplanetapi/domain/PlanetServiceTest.java
+++ src/test/java/com/macedo/swplanetapi/domain/PlanetServiceTest.java
@@ -1,7 +1,9 @@
 package com.macedo.swplanetapi.domain;
 
 import static com.macedo.swplanetapi.common.PlanetConstants.PLANET;
+import static com.macedo.swplanetapi.common.PlanetConstants.INVALID_PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
@@ -40,6 +42,15 @@ public class PlanetServiceTest {
 
         //Assert
         assertThat(sut).isEqualTo(PLANET);
+    }
+
+    @Test
+    public void createPlanet_WithInvalidData_ThrowsException(){
+
+       when(planetService.create(INVALID_PLANET)).thenThrow(RuntimeException.class);
+
+       assertThatThrownBy(()-> planetService.create(INVALID_PLANET)).isInstanceOf(RuntimeException.class);
+
     }
     
 }
