5b5df1b5cadf74b55c70b62c5af8d69c6bc8eb6b
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-01-16 20:21:12 -0400

    Adiciona anotação de id

--- src/main/java/com/macedo/swplanetapi/domain/Planet.java
+++ src/main/java/com/macedo/swplanetapi/domain/Planet.java
@@ -1,14 +1,18 @@
 package com.macedo.swplanetapi.domain;
 
+import org.hibernate.annotations.IdGeneratorType;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "planets")
 public class Planet {
 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
