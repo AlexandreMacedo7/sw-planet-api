66fb96566c8617a54a174dcb0bfafcc55c2c5852
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-01-29 22:48:21 -0400

    Cria método de teste de integração de planet repository

--- src/test/java/com/macedo/swplanetapi/domain/PlanetRepositoryTest.java
+++ src/test/java/com/macedo/swplanetapi/domain/PlanetRepositoryTest.java
@@ -1,7 +1,19 @@
 package com.macedo.swplanetapi.domain;
 
+import static com.macedo.swplanetapi.common.PlanetConstants.PLANET;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 public class PlanetRepositoryTest {
+
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Test
+    public void createPlanet_WithInvalidData_ReturnsPlanet(){
+        Planet planet = planetRepository.save(PLANET);
+    }
 }
