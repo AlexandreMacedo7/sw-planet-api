cd9742a711715d3d4d2c078478ad3f4fc3dd8c65
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-01 20:15:43 -0400

    Implementa método de teste de create Planet

--- src/test/java/com/macedo/swplanetapi/domain/PlanetRepositoryTest.java
+++ src/test/java/com/macedo/swplanetapi/domain/PlanetRepositoryTest.java
@@ -1,10 +1,12 @@
 package com.macedo.swplanetapi.domain;
 
 import static com.macedo.swplanetapi.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 @DataJpaTest
 public class PlanetRepositoryTest {
@@ -12,8 +14,23 @@ public class PlanetRepositoryTest {
     @Autowired
     private PlanetRepository planetRepository;
 
+    //possui tarefas comuns de teste, como persistir, atualizar, encontrar e remover entidades.
+    @Autowired
+    private TestEntityManager testEntityManager;
+
     @Test
     public void createPlanet_WithInvalidData_ReturnsPlanet(){
         Planet planet = planetRepository.save(PLANET);
+
+        Planet sut = testEntityManager.find(Planet.class, planet.getId());
+
+        assertThat(sut).isNotNull();
+
+        //A constante não possui id, é preciso validar cada propriedade
+
+        assertThat(sut.getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getClimente()).isEqualTo(PLANET.getClimente());
+        assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+    
     }
 }
