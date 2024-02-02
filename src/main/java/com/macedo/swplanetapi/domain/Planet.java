3bcf52665860943405c43dfcc7a11dcd25a58431
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-01 22:31:12 -0400

    Adiciona anotação de not empty

--- src/main/java/com/macedo/swplanetapi/domain/Planet.java
+++ src/main/java/com/macedo/swplanetapi/domain/Planet.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "planets")
@@ -17,10 +18,13 @@ public class Planet {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty
     @Column(nullable = false)
     private String name;
+    @NotEmpty
     @Column(nullable = false)
     private String climate;
+    @NotEmpty
     @Column(nullable = false)
     private String terrain;
 
