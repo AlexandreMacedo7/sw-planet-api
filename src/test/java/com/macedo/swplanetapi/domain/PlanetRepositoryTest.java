4aacc0e5ef7900887dfaa7d557d116d42dd497d6
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-02 11:23:36 -0400

    Cria método para teste com nome de planeta já existente

--- src/test/java/com/macedo/swplanetapi/domain/PlanetRepositoryTest.java
+++ src/test/java/com/macedo/swplanetapi/domain/PlanetRepositoryTest.java
@@ -3,6 +3,7 @@ package com.macedo.swplanetapi.domain;
 import static com.macedo.swplanetapi.common.PlanetConstants.PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,33 +16,51 @@ public class PlanetRepositoryTest {
     @Autowired
     private PlanetRepository planetRepository;
 
-    //possui tarefas comuns de teste, como persistir, atualizar, encontrar e remover entidades.
+    // possui tarefas comuns de teste, como persistir, atualizar, encontrar e
+    // remover entidades.
     @Autowired
     private TestEntityManager testEntityManager;
 
     @Test
-    public void createPlanet_WithValidData_ReturnsPlanet(){
+    public void createPlanet_WithValidData_ReturnsPlanet() {
         Planet planet = planetRepository.save(PLANET);
 
         Planet sut = testEntityManager.find(Planet.class, planet.getId());
 
         assertThat(sut).isNotNull();
 
-        //A constante não possui id, é preciso validar cada propriedade
+        // A constante não possui id, é preciso validar cada propriedade
 
         assertThat(sut.getName()).isEqualTo(PLANET.getName());
         assertThat(sut.getClimente()).isEqualTo(PLANET.getClimente());
         assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
-    
+
     }
 
     @Test
-    public void createPlanet_WithInvalidData_ThrowsException(){
+    public void createPlanet_WithInvalidData_ThrowsException() {
 
         Planet emptyPlanet = new Planet();
-        Planet invalidPlanet = new Planet("","","");
+        Planet invalidPlanet = new Planet("", "", "");
 
-        assertThatThrownBy(()-> planetRepository.save(emptyPlanet)).isInstanceOf(RuntimeException.class);
-        assertThatThrownBy(()-> planetRepository.save(invalidPlanet)).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> planetRepository.save(emptyPlanet)).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> planetRepository.save(invalidPlanet)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void createPlanet_WithExistingName_ThrowsException() {
+
+        /*É necessário setar o id, porque para o método save que salva e atualiza, ele confere o id, se existir um, ele apenas atualiza
+         * nesse caso, é preciso colocá-lo como nulo, para poder haver uma nova conferencia de dados.
+         * 
+         * Utiliza-se o detach, para tirar o gerenciamento do Entity Manager sobre a entidade
+         */
+
+        Planet planet = testEntityManager.persistAndFlush(PLANET);
+        testEntityManager.detach(planet);
+        planet.setId(null);
+
+        assertThatThrownBy(() -> planetRepository.save(planet)).isInstanceOf(RuntimeException.class);
+
     }
 }
