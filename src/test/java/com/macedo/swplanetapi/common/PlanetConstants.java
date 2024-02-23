3b704410ad16f7842427e582f9105a76133affa9
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-23 12:16:34 -0400

    Adiciona lista de constantes de planetas

--- src/test/java/com/macedo/swplanetapi/common/PlanetConstants.java
+++ src/test/java/com/macedo/swplanetapi/common/PlanetConstants.java
@@ -1,8 +1,22 @@
 package com.macedo.swplanetapi.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.macedo.swplanetapi.domain.Planet;
 
 public class PlanetConstants {
     public static final Planet PLANET = new Planet("name","climate","terrain");
     public static final Planet INVALID_PLANET = new Planet("","","");
+
+    public static final Planet TATOOINE = new Planet(1L, "Tatooine","arid","desert");
+    public static final Planet ALDERAAN = new Planet(2L,"Alderaan","temperate","grassl");
+    public static final Planet YAVINV = new Planet(3L,"Yavinv", "temperate, tropical","terrain");
+    public static final List<Planet> PLANETS = new ArrayList<>(){
+        {
+            add(TATOOINE);
+            add(ALDERAAN);
+            add(YAVINV);
+        }
+    };
 }
