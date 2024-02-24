f4cc0feb8f2c4e5b52cc3a1ce9c0fb4b6083fb25
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-24 09:55:48 -0400

    Criação de método de listagem de planetas

--- src/main/java/com/macedo/swplanetapi/web/PlanetController.java
+++ src/main/java/com/macedo/swplanetapi/web/PlanetController.java
@@ -1,6 +1,9 @@
 package com.macedo.swplanetapi.web;
 
 import jakarta.validation.Valid;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.macedo.swplanetapi.domain.Planet;
@@ -40,6 +44,13 @@ public class PlanetController {
         return planetService.getByName(name).map(planet -> ResponseEntity.ok(planet))
         .orElseGet(()-> ResponseEntity.notFound().build());
     }
+
+    @GetMapping
+    public ResponseEntity<List<Planet>> list(@RequestParam(required = false) String terrain,
+    @RequestParam(required = false) String climate){
+        List<Planet> planets = planetService.list(terrain, climate);
+        return ResponseEntity.ok(planets);
+    } 
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> remove(@PathVariable("id") Long id){
