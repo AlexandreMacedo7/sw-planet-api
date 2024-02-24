d54d685cdc4d2e0f7553a90ca89d310a39314e6b
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-24 09:56:23 -0400

    Adiciona plenetas e listas de constantes.

--- src/test/java/com/macedo/swplanetapi/common/PlanetConstants.java
+++ src/test/java/com/macedo/swplanetapi/common/PlanetConstants.java
@@ -6,17 +6,17 @@ import java.util.List;
 import com.macedo.swplanetapi.domain.Planet;
 
 public class PlanetConstants {
-    public static final Planet PLANET = new Planet("name","climate","terrain");
-    public static final Planet INVALID_PLANET = new Planet("","","");
+  public static final Planet PLANET = new Planet("name", "climate", "terrain");
+  public static final Planet INVALID_PLANET = new Planet("", "", "");
 
-    public static final Planet TATOOINE = new Planet(1L, "Tatooine","arid","desert");
-    public static final Planet ALDERAAN = new Planet(2L,"Alderaan","temperate","grassl");
-    public static final Planet YAVINV = new Planet(3L,"Yavinv", "temperate, tropical","terrain");
-    public static final List<Planet> PLANETS = new ArrayList<>(){
-        {
-            add(TATOOINE);
-            add(ALDERAAN);
-            add(YAVINV);
-        }
-    };
+  public static final Planet TATOOINE = new Planet(1L, "Tatooine", "arid", "desert");
+  public static final Planet ALDERAAN = new Planet(2L, "Alderaan", "temperate", "grasslands, mountains");
+  public static final Planet YAVINIV = new Planet(3L, "Yavin IV", "temperate, tropical", "jungle, rainforests");
+  public static final List<Planet> PLANETS = new ArrayList<>() {
+    {
+      add(TATOOINE);
+      add(ALDERAAN);
+      add(YAVINIV);
+    }
+  };
 }
