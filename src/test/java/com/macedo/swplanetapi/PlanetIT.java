d39cee7736a57778a8604dbc671d94f099de05a7
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-28 09:27:03 -0400

    Cria perfil de teste e configura acesso ao banco de dados

--- src/test/java/com/macedo/swplanetapi/PlanetIT.java
+++ src/test/java/com/macedo/swplanetapi/PlanetIT.java
@@ -3,10 +3,14 @@ package com.macedo.swplanetapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("it")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class PlanetIT {
     
     @Test
-    public void contextLoads(){}
+    public void contextLoads(){}//Usado para verificar se o contexto está correto
+
+
 }
