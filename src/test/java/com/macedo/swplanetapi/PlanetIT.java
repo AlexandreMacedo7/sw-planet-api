74c6a18d1ffefb129ee36d8649109df650158ee2
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-02-29 11:29:25 -0400

    Cria teste para cenário de sucesso na criação de planeta

--- src/test/java/com/macedo/swplanetapi/PlanetIT.java
+++ src/test/java/com/macedo/swplanetapi/PlanetIT.java
@@ -1,16 +1,38 @@
 package com.macedo.swplanetapi;
 
+import static com.macedo.swplanetapi.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+
+import com.macedo.swplanetapi.domain.Planet;
 
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Sql(scripts = { "/remove_planets.sql" }, executionPhase = ExecutionPhase.AFTER_TEST_METHOD) // limpeza de banco a cada
+                                                                                             // teste
 public class PlanetIT {
-    
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
     @Test
-    public void contextLoads(){}//Usado para verificar se o contexto está correto
+    public void createPlanet_ReturnsCreated() {
+        ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
 
-
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(sut.getBody().getId()).isNotNull();
+        assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
+    }
 }
